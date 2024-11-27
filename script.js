e8843706ff7b7666e35de45d75cf3e15af518c92
PearceJordan <pjb.jordan@gmail.com>
2024-11-27 15:27:49 -0600

    Updated Website

---
+++ script.js
@@ -0,0 +1,22 @@
+// Function to toggle the dropdown
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    
+    // Toggle the display of the dropdown content
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+// Function to toggle the dropdown
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    
+    // Toggle the display of the dropdown content
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
